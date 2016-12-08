1c2dd8c25e372ae83049504519a7aecc733ae316
dodeee <dode.etienne@gmail.com>
2016-12-08 16:10:26 +0100

    Update Utilitaire.java

--- src/Utilitaire/Utilitaire.java
+++ src/Utilitaire/Utilitaire.java
@@ -6,6 +6,23 @@ import java.util.Scanner;
 
 
 public class Utilitaire {
+	
+	    private static final Random RANDOM = new Random();
+
+	    public static int desA() {
+        return RANDOM.nextInt(6)+1;
+    }
+
+          public static int desB() {
+        return RANDOM.nextInt(6)+1;
+    }
+
+          public static int lancerDes(){
+             return desA()+desB();
+             
+              
+          }
+
 
 	
 
